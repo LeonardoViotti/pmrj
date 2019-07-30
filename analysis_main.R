f386c682d71659125910b6859677385d510f28e3
LeonardoViotti <leonardoviotti@gmail.com>
2019-07-30 18:10:31 -0400

    Included new exporting code in analysis_main.R

--- analysis_main.R
+++ analysis_main.R
@@ -142,22 +142,44 @@ feRegSim <- function(form){
 
 # Tabble 2
 r_vd_01 <- feRegSim(Formulas01_str["violent_death_sim"])
+r_vd_01_data <-  regData(r_vd_01, regdf = sr)
+
 r_vr_01 <- feRegSim(Formulas01_str["vehicle_robbery"])
+r_vr_01_data <-  regData(r_vr_01, regdf = sr)
+
 r_rr_01 <- feRegSim(Formulas01_str["street_robbery"])
+r_rr_01_data <-  regData(r_rr_01, regdf = sr)
+
 r_hm_01 <- feRegSim(Formulas01_str["homicide"])
+r_hm_01_data <-  regData(r_hm_01, regdf = sr)
+
 r_pk_01 <- feRegSim(Formulas01_str["dpolice_killing"])
+r_pk_01_data <-  regData(r_pk_01, regdf = sr)
+
 
 # Table 3 - Gaming 
 g_cf_01 <- feRegSim(Formulas01_str["dbody_found"])
+g_cf_01_data <-  regData(g_cf_01, regdf = sr)
+
 g_vt_01 <- feRegSim(Formulas01_str["vehicle_theft"])
+g_vt_01_data <-  regData(g_vt_01, regdf = sr)
+
 g_st_01 <- feRegSim(Formulas01_str["street_theft"])
+g_st_01_data <-  regData(g_st_01, regdf = sr)
 
 
 # Table 4 - Spillovers
 s_or_01 <- feRegSim(Formulas01_str["other_robberies"])
+s_or_01_data <-  regData(s_or_01, regdf = sr)
+
 s_cr_01 <- feRegSim(Formulas01_str["cargo_robbery"])
+s_cr_01_data <-  regData(s_cr_01, regdf = sr)
+
 s_bu_01 <- feRegSim(Formulas01_str["burglary"])
+s_bu_01_data <-  regData(s_bu_01, regdf = sr)
+
 s_sr_01 <- feRegSim(Formulas01_str["store_robbery"])
+s_sr_01_data <-  regData(s_sr_01, regdf = sr)
 
 
 
@@ -165,43 +187,88 @@ s_sr_01 <- feRegSim(Formulas01_str["store_robbery"])
 
 # Tabble 2
 r_vd_02 <- feRegSim(Formulas02_str["violent_death_sim"])
+r_vd_02_data <-  regData(r_vd_02, regdf = sr)
+
 r_vr_02 <- feRegSim(Formulas02_str["vehicle_robbery"])
+r_vr_02_data <-  regData(r_vr_02, regdf = sr)
+
 r_rr_02 <- feRegSim(Formulas02_str["street_robbery"])
+r_rr_02_data <-  regData(r_rr_02, regdf = sr)
+
 r_hm_02 <- feRegSim(Formulas02_str["homicide"])
+r_hm_02_data <-  regData(r_hm_02, regdf = sr)
+
 r_pk_02 <- feRegSim(Formulas02_str["dpolice_killing"])
+r_pk_02_data <-  regData(r_pk_02, regdf = sr)
+
+
 
 # Table 3 - Gaming 
 g_cf_02 <- feRegSim(Formulas02_str["dbody_found"])
+g_cf_02_data <-  regData(g_cf_02, regdf = sr)
+
 g_vt_02 <- feRegSim(Formulas02_str["vehicle_theft"])
+g_vt_02_data <-  regData(g_vt_02, regdf = sr)
+
 g_st_02 <- feRegSim(Formulas02_str["street_theft"])
+g_st_02_data <-  regData(g_st_02, regdf = sr)
 
 
 # Table 4 - Spillovers
 s_or_02 <- feRegSim(Formulas02_str["other_robberies"])
+s_or_02_data <-  regData(s_or_02, regdf = sr)
+
 s_cr_02 <- feRegSim(Formulas02_str["cargo_robbery"])
+s_cr_02_data <-  regData(s_cr_02, regdf = sr)
+
 s_bu_02 <- feRegSim(Formulas02_str["burglary"])
+s_bu_02_data <-  regData(s_bu_02, regdf = sr)
+
 s_sr_02 <- feRegSim(Formulas02_str["store_robbery"])
+s_sr_02_data <-  regData(s_sr_02, regdf = sr)
 
 #### Model 3 2SLS
 
 # Tabble 2
 r_vd_IV <- feRegSim(FormulasIV_str["violent_death_sim"])
+r_vd_IV_data <-  regData(r_vd_IV, regdf = sr)
+
 r_vr_IV <- feRegSim(FormulasIV_str["vehicle_robbery"])
+r_vr_IV_data <-  regData(r_vr_IV, regdf = sr)
+
 r_rr_IV <- feRegSim(FormulasIV_str["street_robbery"])
+r_rr_IV_data <-  regData(r_rr_IV, regdf = sr)
+
 r_hm_IV <- feRegSim(FormulasIV_str["homicide"])
+r_hm_IV_data <-  regData(r_hm_IV, regdf = sr)
+
 r_pk_IV <- feRegSim(FormulasIV_str["dpolice_killing"])
+r_pk_IV_data <-  regData(r_pk_IV, regdf = sr)
+
 
 # Table 3 - Gaming 
 g_cf_IV <- feRegSim(FormulasIV_str["dbody_found"])
+g_cf_IV_data <-  regData(g_cf_IV, regdf = sr)
+
 g_vt_IV <- feRegSim(FormulasIV_str["vehicle_theft"])
+g_vt_IV_data <-  regData(g_vt_IV, regdf = sr)
+
 g_st_IV <- feRegSim(FormulasIV_str["street_theft"])
+g_st_IV_data <-  regData(g_st_IV, regdf = sr)
 
 
 # Table 4 - Spillovers
 s_or_IV <- feRegSim(FormulasIV_str["other_robberies"])
+s_or_IV_data <-  regData(s_or_IV, regdf = sr)
+
 s_cr_IV <- feRegSim(FormulasIV_str["cargo_robbery"])
+s_cr_IV_data <-  regData(s_cr_IV, regdf = sr)
+
 s_bu_IV <- feRegSim(FormulasIV_str["burglary"])
+s_bu_IV_data <-  regData(s_bu_IV, regdf = sr)
+
 s_sr_IV <- feRegSim(FormulasIV_str["store_robbery"])
+s_sr_IV1_data <-  regData(s_sr_IV, regdf = sr)
 
 
 #------------------------------------------------------------------------------#
@@ -218,64 +285,96 @@ RegPoisson <- function(form){
 }
 
 p_vd <- RegPoisson(Formulas_poi_str["violent_death_sim"])
+p_vd_data <-  regData(p_vd, regdf = sr)
+
 p_vr <- RegPoisson(Formulas_poi_str["vehicle_robbery"])
+p_vr_data <-  regData(p_vr, regdf = sr)
+
 p_rr <- RegPoisson(Formulas_poi_str["street_robbery"])
+p_rr_data <-  regData(p_rr, regdf = sr)
+
 p_po <- RegPoisson(Formulas_poi_str["dpolice_killing"])
+p_po_data <-  regData(p_po, regdf = sr)
 
 
 #------------------------------------------------------------------------------#
 ##### Export ####
 
-indepVar_label <- c("On target" = "on_target")
-indepVar_label_pla <- c("On target" = "on_target_plapre")
+#### Define commun elements
+n_aisp_line_9 <- c("Number of aisp", rep("39", 9))
+chifeFE_line_9 <- c("Chief FE", rep(c( "No", "Yes", "Yes"), 3))
+chifeFE_line_12 <- c("Chief FE", rep(c( "No", "Yes", "Yes"), 4))
 
-stats_labels <- c("Observations" = "nobs",  
-                  "R2 adjusted" = "adj.r.squared")
-
-#models_labels <- c("Violent Death", "Violent Death", "Carjacking", "Carjacking")
+indepVar_label <- "On target"
+col_labels_9 <- rep(c("OLS",	"OLS",	"2SLS"), 3)
 
 
-models_labels <- c("Model 1" = "OLS", 
-                   "Model 2" = "OLS", 
-                   "Model 3" = "IV", 
-                   "Model 4" = "OLS",
-                   "Model 5" = "OLS", 
-                   "Model 6" = "IV",
-                   "Model 7" = "OLS", 
-                   "Model 8" = "OLS", 
-                   "Model 9" = "IV",
-                   "Model 10" = "OLS", 
-                   "Model 11" = "OLS", 
-                   "Model 12" = "IV")
 
+#### Define formatting functions
+
+# Function to find dep var means of regressions
+Ymean <- function(x){
+  mean(regData(x, sr)[,regDepVars(x)])
+}
+
+# Function to create the row for regression tables
+Ymean_row <- function(list){
+  c("Y mean", sapply(bar, Ymean) %>% round(2))
+}
+
+
+# Export function
+createTable <- function(reg_list, 
+                        add_lines_list,
+                        title,
+                        dep_var_labels,
+                        outPath){
+  stargazer(reg_list,
+            keep = ("on_target"),
+            covariate.labels = "On target",
+            dep.var.labels = dep_var_labels,
+            title = title,
+            dep.var.caption  = "Number  of  occurrences",
+            column.labels   = col_labels_9,
+            add.lines = add_lines_list,
+            digits = 3,
+            omit.stat = c("rsq","ser", "f"),
+            out = outPath,
+            type = "html")
+}
 
 
 # Table 2
-tab2 <- 
-  export_summs(r_vd_01, 
-               r_vd_02, 
-               r_vd_IV,
-               r_vr_01, 
-               r_vr_02,
-               r_vr_IV,
-               r_rr_01, 
-               r_rr_02,
-               r_rr_IV,
-               digits = 3,
-               scale = TRUE,
-               coefs = indepVar_label,
-               statistics = stats_labels,
-               model.names = models_labels[1:9] #,
-               # to.file ="xlsx",
-               # file.name = file.path(OUTPUTS,"tab2.xlsx")
-               )
+tab2_regs <- 
+  list(r_vd_01, 
+       r_vd_02, 
+       r_vd_IV,
+       r_vr_01, 
+       r_vr_02,
+       r_vr_IV,
+       r_rr_01, 
+       r_rr_02,
+       r_rr_IV)
+
+tab2_addLines <- list(chifeFE_line,
+                   Ymean_row(tab2_regs),
+                   n_aisp_line_9)
+
+
+createTable(reg_list = tab2_regs,
+            add_lines_list = tab2_addLines,
+            dep_var_labels = c("Violent deaths", 
+                               "Vehicle robbery (Carjacking)",	
+                               "Street robbery"),
+            title = "Table 2 – Effect of expectancy of receiving bonuses on crime rates",
+            outPath = file.path(OUTPUTS, "tab2.html"))
 
 
 
 
 # Table 3
-tab3 <- 
-  export_summs(g_cf_01, 
+tab3_regs <- 
+  list(g_cf_01, 
                g_cf_02, 
                g_cf_IV, 
                g_vt_01, 
@@ -283,22 +382,13 @@ tab3 <-
                g_vt_IV,
                g_st_01, 
                g_st_02,
-               g_st_IV,
-               digits = 3,
-               scale = TRUE,
-               transform.response = T,
-               coefs = indepVar_label,
-               model.names = models_labels[1:9],
-               statistics = stats_labels #,
-               # to.file ="xlsx",
-               # file.name = file.path(OUTPUTS,"tab3.xlsx")
-               )
+               g_st_IV)
 
 
 
 # Table 4
-tab4 <- 
-  export_summs(s_or_01, 
+tab4_regs <- 
+  list(s_or_01, 
                s_or_02, 
                s_or_IV, 
                s_cr_01, 
@@ -309,32 +399,14 @@ tab4 <-
                s_bu_IV,
                s_sr_01,
                s_sr_02,
-               s_sr_IV,
-               digits = 3,
-               scale = TRUE,
-               transform.response = T,
-               coefs = indepVar_label,
-               model.names = models_labels,
-               statistics = stats_labels #,
-               # to.file ="xlsx",
-               # file.name = file.path(OUTPUTS,"tab4.xlsx")
-               )
+               s_sr_IV)
 
 # Poisson model
-# tab5 <- 
-#   export_summs(p_vd, 
-#                p_vr,
-#                p_rr,
-#                p_po,
-#                digits = 3,
-#                scale = TRUE,
-#                transform.response = T,
-#                coefs = indepVar_label,
-#                model.names = models_labels,
-#                statistics = stats_labels #,
-#                # to.file ="xlsx",
-#                # file.name = file.path(OUTPUTS,"tab4.xlsx")
-#   )
+tab5_regs <-
+  list(p_vd,
+       p_vr,
+       p_rr,
+       p_po)
 
 
 
@@ -410,7 +482,6 @@ if(EXPORT_tables){
 
 #------------------------------------------------------------------------------#
 #### Monthly coef graphs ####
-
 
 #### Create Variables
 
