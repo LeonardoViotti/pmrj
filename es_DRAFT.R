16747deda439d6b4513177ed871e8e810ad1ff8a
LeonardoViotti <leonardoviotti@gmail.com>
2020-01-24 15:55:04 -0500

    First draft plot for event study analysis.

--- es_DRAFT.R
+++ es_DRAFT.R
@@ -24,6 +24,9 @@ vars <-
     "year",
     "year_month",
     "semester",
+    "violent_death_sim",
+    "vehicle_robbery",
+    "street_robbery",
     "violent_death_sim_cum",
     "vehicle_robbery_cum",
     "street_robbery_cum",
@@ -79,10 +82,57 @@ hit_month <- function(target, t){
 esdf %<>% 
   group_by(aisp, year, semester) %>% 
   mutate(hit_month = hit_idx(on_target,year_month),
-         t = unclass(year_month) - unclass(hit_month)+1)
+         t = unclass(year_month) - unclass(hit_month)+1,
+         t = as.factor(t))
 
 #------------------------------------------------------------------------------#
 #### ES models ####
 
+eslm_vd <- felm(violent_death_sim ~ t | aisp | 0 | 0,
+                data = esdf)
+
+
+
 #------------------------------------------------------------------------------#
 #### ES plots ####
+
+
+# Plot DF
+eslm_vd_df <- data.frame( coeff = eslm_vd$coeff,
+                          se = eslm_vd$se)  
+
+names(eslm_vd_df) <- c("coeff", "se") # not sure why it doesnt work simply using "coeff =" above
+
+eslm_vd_df$monthsFromTreat <- row.names(eslm_vd_df)
+
+# Fix variable names
+eslm_vd_df$monthsFromTreat <- as.integer(sub("t", "", eslm_vd_df$monthsFromTreat))
+eslm_vd_df$monthsFromTreat <- as.ordered(eslm_vd_df$monthsFromTreat)   
+
+
+# Plot
+plot_def <- function(regdata, title, xlabel){
+  
+  ggplot(regdata, aes(x=monthsFromTreat, y=coeff)) +
+    geom_point(col = "navyblue")+
+    geom_vline(xintercept=6.5) +
+    geom_errorbar(aes(ymin = coeff - se, ymax = coeff + se),  col = "navyblue", width=.2)+
+    
+    xlab(xlabel) +
+    
+    theme(
+      legend.title=element_blank(),
+      panel.background = element_blank(),
+      panel.grid.major =element_line(colour = "gray90"),
+      panel.grid.minor =element_line(colour = "gray90"),
+      plot.title = element_text(hjust = .5)) +
+    ggtitle(title)
+  
+}
+
+
+reg_plot <-  plot_def(eslm_vd_df, # data
+                      "foo", # title
+                      "bar") # x label
+
+
