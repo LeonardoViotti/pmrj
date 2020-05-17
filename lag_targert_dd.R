910076e7b61aab22078291d677a643a204353bbf
LeonardoViotti <leonardoviotti@gmail.com>
2020-05-17 18:19:14 -0400

    Some progress in replicating the original t10 analysis.

--- lag_targert_dd.R
+++ lag_targert_dd.R
@@ -20,8 +20,8 @@
 
 #------------------------------------------------------------------------------#
 # Load data
-# sm <- raw_data
-sm <- final_data
+sm <- raw_data
+# sm <- final_data
 
 #------------------------------------------------------------------------------#
 # Create lagged variable
@@ -36,7 +36,7 @@ sm <- final_data
 sm %<>% 
   mutate(
     # Still within the semester target for each crime 
-    hit_violent_death = as.integer(violent_death_sim_cum <=target_vd_sem), 
+    hit_violent_death = as.integer(violent_death_sim_cum <= target_vd_sem), 
     hit_street_robbery = as.integer(street_robbery_cum <= target_sr_sem),
     hit_vehicle_robbery = as.integer(vehicle_robbery_cum <= target_vr_sem),
     
@@ -60,8 +60,11 @@ sm %<>%
   mutate(last_month_hit = last_month*hit_month_l)
 
 # Create the exact same variable from the original code to test
-sm %>% 
-  mutate(hit_target = )
+sm %<>% 
+  mutate(hit_target = as.numeric(on_target ==1),
+         hit_target = ifelse(cycle == 1,
+                             as.numeric(hit_month_l ==1),
+                             hit_target))
 
 
 
@@ -95,6 +98,50 @@ sm %>%
 # ) %>% View
 
 
+#------------------------------------------------------------------------------#
+# Replicate the original analysis exactly
+
+foo <- 
+  raw_data %>% 
+  mutate(last_moth  = ifelse(month %in% c(6,12),
+                             1,
+                             0)) %>% 
+  # Create semester sums of all crime variables adding suffix 6
+  group_by(aisp, sem_year) %>% 
+  mutate_at( c(depVars, "target_vd", "target_sr", "target_vr"),
+             .funs = list("6" = ~sum(., na.rm = T))
+  ) %>% 
+  
+  # Other varaibles that don't need to be grouped
+  ungroup() %>%
+  mutate(
+    
+    # Create award variables
+    award_violent_death = as.integer(violent_death_sim_6<=target_vd_6), 
+    award_street_robbery = as.integer(street_robbery_6<= target_sr_6),
+    # Petential error in original code this was not using the sum var
+    award_vehicle_robbery = as.integer(vehicle_robbery_6 <= target_vr_6),
+    
+    # If awarded in either crime varaible
+    awarded= as.integer(award_violent_death==1 & 
+                          award_street_robbery==1 & 
+                          award_vehicle_robbery==1)
+  ) %>% 
+  # Hit_target varaible
+  group_by(aisp) %>%
+  mutate(hit_target = ifelse(cycle == 1,
+                             lag(awarded) ,
+                             on_target),
+         awarded_l = lag(awarded, 1)) %>% 
+  ungroup()
+
+
+foo %>%  subset(sem_year>100 & month %in% c(6,7,12,1)) %>% select(hit_target) %>% summary
+
+sm  %>%  subset(sem_year>100 & month %in% c(6,7,12,1)) %>% select(hit_target) %>% summary
+sm  %>%  subset(sem_year>100) %>% select(hit_violent_death) %>% summary
+
+  
 #------------------------------------------------------------------------------#
 # Regression
 
