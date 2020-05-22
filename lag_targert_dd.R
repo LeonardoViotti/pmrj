059a807d7219fa120b91428440ed0247232e5d88
LeonardoViotti <leonardoviotti@gmail.com>
2020-05-22 14:16:33 -0400

    Small changes to lag_target analysis.

--- lag_targert_dd.R
+++ lag_targert_dd.R
@@ -98,49 +98,6 @@ sm %<>%
 # ) %>% View
 
 
-#------------------------------------------------------------------------------#
-# Replicate the original analysis exactly
-
-foo <- 
-  raw_data %>% 
-  mutate(last_moth  = ifelse(month %in% c(6,12),
-                             1,
-                             0)) %>% 
-  # Create semester sums of all crime variables adding suffix 6
-  group_by(aisp, sem_year) %>% 
-  mutate_at( c(depVars, "target_vd", "target_sr", "target_vr"),
-             .funs = list("6" = ~sum(., na.rm = T))
-  ) %>% 
-  
-  # Other varaibles that don't need to be grouped
-  ungroup() %>%
-  mutate(
-    
-    # Create award variables
-    award_violent_death = as.integer(violent_death_sim_6<=target_vd_6), 
-    award_street_robbery = as.integer(street_robbery_6<= target_sr_6),
-    # Petential error in original code this was not using the sum var
-    award_vehicle_robbery = as.integer(vehicle_robbery_6 <= target_vr_6),
-    
-    # If awarded in either crime varaible
-    awarded= as.integer(award_violent_death==1 & 
-                          award_street_robbery==1 & 
-                          award_vehicle_robbery==1)
-  ) %>% 
-  # Hit_target varaible
-  group_by(aisp) %>%
-  mutate(hit_target = ifelse(cycle == 1,
-                             lag(awarded) ,
-                             on_target),
-         awarded_l = lag(awarded, 1)) %>% 
-  ungroup()
-
-
-foo %>%  subset(sem_year>100 & month %in% c(6,7,12,1)) %>% select(hit_target) %>% summary
-
-sm  %>%  subset(sem_year>100 & month %in% c(6,7,12,1)) %>% select(hit_target) %>% summary
-sm  %>%  subset(sem_year>100) %>% select(hit_violent_death) %>% summary
-
   
 #------------------------------------------------------------------------------#
 # Regression
@@ -150,16 +107,24 @@ sm_reg <- sm %>%
   subset(sem_year>100) %>% 
   subset(month %in% c(6,7,12,1))
 
-sm_reg$y <- sm_reg$violent_death_sim
+sm_reg$y <- sm_reg$dbody_found
 
 
 felm(y ~ last_month_hit +  hit_month_l +  last_month  + 
     policemen_aisp + policemen_upp + n_precinct + max_prize +
-    population | month + year | 0 | 0,
-    data = sm_reg) %>% summary()
+    population | month + year + aisp | 0 | 0,
+    data = sm_reg) %>% stargazer(type = 'text') 
+
+felm(y ~ last_month_hit +  hit_month_l +  last_month  + 
+       policemen_aisp + policemen_upp + n_precinct + max_prize +
+       population | month + year + aisp + id_cmt | 0 | 0,
+     data = sm_reg) %>% stargazer(type = 'text') 
 
 
-
+felm(y ~ last_month_hit +  hit_month_l +  last_month  + 
+       policemen_aisp + policemen_upp + n_precinct + max_prize +
+       population | year + aisp + id_cmt | 0 | 0,
+     data = sm_reg) %>% stargazer(type = 'text') 
 
 
 
