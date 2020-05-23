bccb9c8c3b77597e553fa837a717187cc3d8f736
LeonardoViotti <leonardoviotti@gmail.com>
2020-05-22 20:50:56 -0400

    First draft of lag dd tables in R.

--- lag_targert_dd.R
+++ lag_targert_dd.R
@@ -31,7 +31,6 @@ sm <- raw_data
 # create a variable (to be lagged) that means the aisp is still within the semester
 # target for all three crimes.
 
-
 # Create regression variables
 sm %<>% 
   mutate(
@@ -59,12 +58,170 @@ sm %<>%
   # Interaction
   mutate(last_month_hit = last_month*hit_month_l)
 
-# Create the exact same variable from the original code to test
-sm %<>% 
-  mutate(hit_target = as.numeric(on_target ==1),
-         hit_target = ifelse(cycle == 1,
-                             as.numeric(hit_month_l ==1),
-                             hit_target))
+
+
+#------------------------------------------------------------------------------#
+##### Regression data set ### 
+
+# Create a data set with only target months
+sm_reg <- sm %>% 
+  subset(sem_year>100) %>% 
+  subset(month %in% c(6,7,12,1))
+
+  
+#------------------------------------------------------------------------------#
+#### Regression formulas ####
+
+#---------------#
+# Set variables #
+
+
+indep_vars_dd <- c("last_month_hit",
+                   "hit_month_l",
+                   "last_month",
+                   "policemen_aisp",
+                   "policemen_upp",
+                   "n_precinct",
+                   "max_prize",
+                   "population" )
+
+# Since there are only 4 months in this analysis I'm removing month
+# fixed effects to avoid collinearity
+FE_vars_dd <- c("aisp",
+                "year", 
+                "id_cmt")
+
+# Set cluster SE level
+cluster_vars_dd= "0"
+
+
+#--------------#
+# Set formulas #
+
+# Function to create formulas
+reg_formula <- function(dep_vars,
+                        indep_vars,
+                        FE_vars,
+                        instr_vars = 0,
+                        custer_vars = 0){
+  
+  paste_plus <- function(x){
+    paste(x, collapse =  " + ")
+  }
+
+  # Set regression, FEs, cluster SEs and IV
+  paste_config <- function(FE_vars,
+                           custer_vars,
+                           instr_vars){
+    paste(" ", 
+          paste_plus(FE_vars), 
+          paste_plus(instr_vars), 
+          paste_plus(custer_vars),
+          sep = " | ")
+  }
+  
+  # Combine all elements
+  final_formula <- paste(dep_vars, 
+                         paste(paste_plus(indep_vars_dd),
+                               paste_config(FE_vars,
+                                            custer_vars,
+                                            instr_vars)), 
+                         sep = " ~ ")
+  
+  
+  # Named vector with the dependent variables
+  names(final_formula) <- dep_vars
+  
+  
+  # Return named vector of formulas
+  return(final_formula)
+}
+
+
+# Second model with chief FE
+dd_formulas_m2 <- 
+  reg_formula(depVars,
+              indep_vars_dd,
+              FE_vars_dd)
+
+# First model without chief FE
+dd_formulas_m1 <- 
+  reg_formula(depVars,
+              indep_vars_dd,
+              FE_vars_dd[1:2])
+
+#------------------------------------------------------------------------------#
+#### Run regressions #### 
+
+# Set regressions model formula
+feRegSim <- function(dep_var,
+                     model = 1,
+                     formula_vector1 = dd_formulas_m1,
+                     formula_vector2 = dd_formulas_m2,
+                     data = sm_reg){
+  if(model ==1){
+    form <- formula_vector1[dep_var]
+  } else{
+    form <- formula_vector2[dep_var]
+  }
+  
+
+  form <- as.formula(form)
+  model <- felm(form, data = data, keepCX = T)
+  
+  # Return regression object
+  return(model)
+  
+}
+
+
+tab2 <- 
+  stargazer(
+    feRegSim('violent_death_sim', model = 1),
+    feRegSim('violent_death_sim', model = 2),
+    feRegSim('vehicle_robbery', model = 1),
+    feRegSim('vehicle_robbery', model = 2),
+    feRegSim('street_robbery', model = 1),
+    feRegSim('street_robbery', model = 2),
+    keep = c("last_month_hit",
+             "hit_month_l",
+             "last_month"),
+    omit.stat=c("LL","ser","f"),
+    type = 'text')
+
+tab_gaming <- 
+  stargazer(
+    feRegSim('dbody_found', model = 1),
+    feRegSim('dbody_found', model = 2),
+    feRegSim('vehicle_theft', model = 1),
+    feRegSim('vehicle_theft', model = 2),
+    feRegSim('street_theft', model = 1),
+    feRegSim('street_theft', model = 2),
+    feRegSim('dpolice_killing', model = 1),
+    feRegSim('dpolice_killing', model = 2),
+
+    keep = c("last_month_hit",
+             "hit_month_l",
+             "last_month"),
+    omit.stat=c("LL","ser","f"),
+    type = 'text')
+
+
+
+#------------------------------------------------------------------------------#
+# draft
+
+
+
+# sm_reg$y <- sm_reg$violent_death_sim
+# 
+# 
+# felm(y ~ last_month_hit +  hit_month_l +  last_month  +
+#     policemen_aisp + policemen_upp + n_precinct + max_prize +
+#     population | year + aisp | 0 | 0,
+#     data = sm_reg) %>% stargazer(type = 'text')
+# 
+# 
 
 
 
@@ -96,41 +253,6 @@ sm %<>%
 #               "hit_month",
 #               "hit_month_l"
 # ) %>% View
-
-
-  
-#------------------------------------------------------------------------------#
-# Regression
-
-# Create a data set with only target months
-sm_reg <- sm %>% 
-  subset(sem_year>100) %>% 
-  subset(month %in% c(6,7,12,1))
-
-sm_reg$y <- sm_reg$dbody_found
-
-
-felm(y ~ last_month_hit +  hit_month_l +  last_month  + 
-    policemen_aisp + policemen_upp + n_precinct + max_prize +
-    population | month + year + aisp | 0 | 0,
-    data = sm_reg) %>% stargazer(type = 'text') 
-
-felm(y ~ last_month_hit +  hit_month_l +  last_month  + 
-       policemen_aisp + policemen_upp + n_precinct + max_prize +
-       population | month + year + aisp + id_cmt | 0 | 0,
-     data = sm_reg) %>% stargazer(type = 'text') 
-
-
-felm(y ~ last_month_hit +  hit_month_l +  last_month  + 
-       policemen_aisp + policemen_upp + n_precinct + max_prize +
-       population | year + aisp + id_cmt | 0 | 0,
-     data = sm_reg) %>% stargazer(type = 'text') 
-
-
-
-
-
-
 
 
 
